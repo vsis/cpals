d9b8c81ad8ac63c288365d3160c9018601794027
Roddy González <roddy.gonzalez.89@gmail.com>
2019-09-16 11:02:47 -0300

    Fixed functions comments

--- src/text_bin.h
+++ src/text_bin.h
@@ -1,14 +1,14 @@
-// transforms two hexadecimal chars to a byte
+// transforms one hexadecimal char to a byte
 unsigned char single_hex_to_byte(char a);
 
 // transforms a string of hexadecimals to a byte array
 void hex_to_bytes(char * hexs, unsigned char * bytes, int max_bytes);
 
-// given a byte and a rest, returns a base46 char and the new rest
+// given a byte, returns a base46 char
 char byte_to_base64(unsigned char byte);
 
-// given 3 bytes returns 4 to 6 base64 chars.
-// the number of base64 chars depends on number of trailing zeroes
+// given 3 bytes and the number of trailing zeroes, returns 4 base64 chars
+//  trailing zeroes are the number of 0x00 at the end of bytes that shouldn't be counted
 void three_bytes_to_base64(unsigned char * bytes, char * base64, unsigned char trailing_zeroes );
 
 // transforms a bytes array to a base64 string
